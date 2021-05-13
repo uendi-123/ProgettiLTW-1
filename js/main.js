389d8394ceae95bd67f0f3687feb04edb2938f14
Pippinu <iacono760@gmail.com>
2021-05-13 19:02:53 +0200

    Risolvere .load
    .load in main.js ricarica la classe card sovrascrivendo il BG di normalCard, PORCODDIO

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
-    $('.col').click(function(){
-        $('.col').removeClass('activeCard');
-        $('.col').children('span').removeClass('activeSpanCard');
-        $(this).toggleClass('activeCard');
-        $(this).children('span').toggleClass('activeSpanCard')
+    $('#dinamicDiv').load('../html/offerte.html');
+    
+    $('.normalCard').click(function(){
+        $('.normalCard').removeClass('activeCard');
+        $(this).addClass('activeCard');
+
+        $('.normalCard').children('span').removeClass('activeSpanCard');
+        $(this).children('span').addClass('activeSpanCard');
+
+        if($(this).attr('id') === 'offerteDiv'){
+            $('#dinamicDiv').load('../html/offerte.html');
+        }
     })
 })
