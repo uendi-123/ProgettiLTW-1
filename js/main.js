2ddab903864963149da58677dc42c1647d984015
Pippinu <iacono760@gmail.com>
2021-05-13 17:31:55 +0200

    Try

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.col').click(function(){
-        $('.col').removeClass('activeCard');
-        $('.col').children('span').removeClass('activeSpanCard');
-        $(this).toggleClass('activeCard');
-        $(this).children('span').toggleClass('activeSpanCard')
-    })
-})
