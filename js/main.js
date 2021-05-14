218295fd2f65346749c0206b7f804bae1a95f1bd
Pippinu <iacono760@gmail.com>
2021-05-14 18:09:12 +0200

    Aggiunti Form Login e Registrazione
    -Sistemare SignUp form che scompare
    -Aggiungere API Calendario su Data Nascita
    -JS Login e Form Registrazione
    -Sistemare Card
    -Tasto 'i miei ordini' solo se fatto accesso
    -Orario in input Data
    
    -Pagina Web auto per Citta
    -Pagina/Barra degli ordini

--- js/main.js
+++ js/main.js
@@ -2,11 +2,24 @@ $(document).ready(function(){
     $('#dinamicDiv').load('../html/offerte.html');
     
     $('.normalCard').click(function(){
-        $('.normalCard').removeClass('activeCard');
-        $(this).addClass('activeCard');
+        // $('.normalCard').removeClass('activeCard');
+        // $(this).addClass('activeCard');
 
         $('.normalCard').children('span').removeClass('activeSpanCard');
         $(this).children('span').addClass('activeSpanCard');
+
+        $('.normalCard').children('span').removeClass('activeSpanCard');
+
+        // for(let i = 0; i < $('.normalCard').length; i++){
+        //     if($('.normalCard').get(i).children('span').hasClass('fs-2') && $('.normalCard')[i] !== $(this)){
+        //         $('.normalCard')[i].children('span').removeClass('fs-2');
+        //         $('.normalCard')[i].children('span').addClass('fs-4');
+
+        //         $(this).children('span').removeClass('fs-4');
+        //         $(this)[i].children('span').addClass('fs-2');
+        //     }
+        // }
+        // $(this).children('span').addClass('fs-2');
 
         if($(this).attr('id') === 'offerteDiv'){
             $('#dinamicDiv').load('../html/offerte.html');
