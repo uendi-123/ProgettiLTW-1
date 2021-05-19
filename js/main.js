44ae454f0b0a3ca25db2422034948210eb3aba19
Pippinu <iacono760@gmail.com>
2021-05-19 11:50:21 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#dinamicDiv').load('../html/offerte.html');
+    //$('#dinamicDiv').load('../html/offerte.html');
     //Input che fanno uso dell'API Datepicker
     $('#dateStart').datepicker();
     $('#dateEnd').datepicker();
@@ -47,30 +47,30 @@ $(document).ready(function(){
         }
     });
 
-    $('.normalCard').click(function(){
-        $('.normalCard').removeClass('activeCard');
-        $(this).addClass('activeCard');
+    // $('.normalCard').click(function(){
+    //     $('.normalCard').removeClass('activeCard');
+    //     $(this).addClass('activeCard');
 
-        $('.normalCard').children('span').removeClass('activeSpanCard');
-        $(this).children('span').addClass('activeSpanCard');
+    //     $('.normalCard').children('span').removeClass('activeSpanCard');
+    //     $(this).children('span').addClass('activeSpanCard');
 
-        $('.normalCard').children('span').removeClass('activeSpanCard');
+    //     $('.normalCard').children('span').removeClass('activeSpanCard');
 
-        for(let i = 0; i < $('.normalCard').length; i++){
-            if($('.normalCard').get(i).children('span').hasClass('fs-2') && $('.normalCard')[i] !== $(this)){
-                $('.normalCard')[i].children('span').removeClass('fs-2');
-                $('.normalCard')[i].children('span').addClass('fs-4');
+    //     for(let i = 0; i < $('.normalCard').length; i++){
+    //         if($('.normalCard').get(i).children('span').hasClass('fs-2') && $('.normalCard')[i] !== $(this)){
+    //             $('.normalCard')[i].children('span').removeClass('fs-2');
+    //             $('.normalCard')[i].children('span').addClass('fs-4');
 
-                $(this).children('span').removeClass('fs-4');
-                $(this)[i].children('span').addClass('fs-2');
-            }
-        }
-        $(this).children('span').addClass('fs-2');
+    //             $(this).children('span').removeClass('fs-4');
+    //             $(this)[i].children('span').addClass('fs-2');
+    //         }
+    //     }
+    //     $(this).children('span').addClass('fs-2');
 
-        if($(this).attr('id') === 'offerteDiv'){
-            $('#dinamicDiv').load('../html/offerte.html');
-        }
-    });
+    //     if($(this).attr('id') === 'offerteDiv'){
+    //         $('#dinamicDiv').load('../html/offerte.html');
+    //     }
+    // });
 
     //SignUp Validation
     //La seguente istruzione mette nell'array tutti gli elem <input> del form
